11bbbee45849f11d7d51778db80b049a8c5e64fc
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-03-07 00:34:04 -0500

    spread operator in props (new way)

--- src/index.js
+++ src/index.js
@@ -3,47 +3,44 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
-const firstBook = {
-  title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth and Tim Warnes",
-  img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg",
-};
-const secondBook = {
-  title: "Sold on a Monday",
-  author: "ristina McMorris",
-  img: "https://m.media-amazon.com/images/I/81bD6Ll-seL._AC_UL320_.jpg",
-};
+const books = [
+  {
+    id: 1,
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth and Tim Warnes",
+    img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg",
+  },
+  {
+    id: 2,
+    title: "Sold on a Monday",
+    author: "ristina McMorris",
+    img: "https://m.media-amazon.com/images/I/81bD6Ll-seL._AC_UL320_.jpg",
+  },
+  {
+    id: 3,
+    title: "Love You Forever",
+    author: "Robert Munsch and Sheila McGraw",
+    img: "https://m.media-amazon.com/images/I/81vpsIs58WL._AC_UL320_.jpg",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-          suscipit eius dolores eaque enim, a laudantium commodi nobis illum
-          assumenda numquam doloribus, voluptatem quae itaque tenetur facilis
-          doloremque libero culpa.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={firstBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} alt="Book image" />
-      <h1>{props.title}</h1>
-      <h4>{props.author.toUpperCase()}</h4>
+      <img src={img} alt="Book image" />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
       {props.children}
     </article>
   );
