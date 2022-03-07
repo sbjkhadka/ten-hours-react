00ffe2bca6e04ae44dd5cf5062075ff9330838d0
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-03-07 14:12:07 -0500

    event basics

--- src/index.js
+++ src/index.js
@@ -35,13 +35,32 @@ function BookList() {
 }
 
 const Book = (props) => {
+  // attribute, event handler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("Hello World");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   const { img, title, author } = props;
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="Book image" />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
       {props.children}
+      <button type="button" onClick={clickHandler}>
+        Reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More complex example
+      </button>
     </article>
   );
 };
